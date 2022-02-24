2537685feab43a76054ae995dc4f9e2a049ab23a
Sreenath285 <sreenath.285@gmail.com>
2022-02-24 07:44:42 +0530

    Welcome to Address Book Program

---
+++ AddressBookSystem/src/AddressBook.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
